01b1f0336424a0a9b6015f8cd5936483b0d2601b
lemon <snyxmk@gmail.com>
2025-04-23 18:46:11 +0530

    basic product service done

--- product/cmd/start.go
+++ product/cmd/start.go
@@ -1,7 +1,64 @@
 package main
 
-import scrips "github.com/lmnzx/slopify/product/scripts"
+import (
+	"context"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	auth "github.com/lmnzx/slopify/auth/proto"
+	"github.com/lmnzx/slopify/pkg/middleware"
+	"github.com/lmnzx/slopify/product/handler"
+	"github.com/lmnzx/slopify/product/repository"
+	"github.com/meilisearch/meilisearch-go"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
 
 func main() {
-	scrips.Seed()
+	log := middleware.GetLogger()
+
+	dbpool, err := pgxpool.New(context.Background(), "postgresql://postgres:postgres@localhost:5432/slopify?sslmode=disable")
+	if err != nil {
+		log.Fatal().Err(err).Msg("unable to connect to database")
+	}
+	defer dbpool.Close()
+
+	if err := dbpool.Ping(context.Background()); err != nil {
+		log.Fatal().Err(err).Msg("unable to ping database")
+	}
+
+	client := meilisearch.New("http://localhost:7700", meilisearch.WithAPIKey("masterkey"))
+	defer client.Close()
+
+	index := client.Index("products")
+
+	queries := repository.New(dbpool)
+
+	conn, err := grpc.NewClient(":8000", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to connect")
+	}
+	defer conn.Close()
+
+	c := auth.NewAuthServiceClient(conn)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go handler.StartRestServer(ctx, queries, index, c, &wg)
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+
+	<-sigCh
+	log.Info().Msg("received shutdown signal")
+	cancel()
+
+	wg.Wait()
 }
---
+++ product/handler/rest.go
@@ -0,0 +1,106 @@
+package handler
+
+import (
+	"context"
+	"sync"
+
+	"github.com/fasthttp/router"
+	auth "github.com/lmnzx/slopify/auth/proto"
+	"github.com/lmnzx/slopify/pkg/middleware"
+	"github.com/lmnzx/slopify/pkg/response"
+	"github.com/lmnzx/slopify/product/repository"
+	"github.com/meilisearch/meilisearch-go"
+	"github.com/rs/zerolog"
+	"github.com/valyala/fasthttp"
+)
+
+type RestHandler struct {
+	index   meilisearch.IndexManager
+	queries *repository.Queries
+	res     *response.ResponseSender
+	log     zerolog.Logger
+}
+
+func NewRestHandler(queries *repository.Queries, index meilisearch.IndexManager) *RestHandler {
+	return &RestHandler{
+		index:   index,
+		queries: queries,
+		log:     middleware.GetLogger(),
+		res:     response.NewResponseSender(),
+	}
+}
+
+func StartRestServer(ctx context.Context, queries *repository.Queries, index meilisearch.IndexManager, auth auth.AuthServiceClient, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	r := router.New()
+
+	handler := NewRestHandler(queries, index)
+	authMw := middleware.AuthMiddleware(auth)
+
+	r.GET("/health", handler.healthCheck)
+	r.GET("/get", authMw(handler.getProduct))
+
+	server := &fasthttp.Server{
+		Handler: middleware.RequestLogger(r.Handler),
+	}
+
+	log := middleware.GetLogger()
+	serveErrCh := make(chan error, 1)
+	go func() {
+		restAddr := ":9002"
+		log.Info().Str("address", restAddr).Msg("rest server started")
+		if err := server.ListenAndServe(restAddr); err != nil {
+			select {
+			case <-ctx.Done():
+				log.Println("fasthttp server stopped gracefully")
+			default:
+				serveErrCh <- err
+			}
+			close(serveErrCh)
+		} else {
+			close(serveErrCh)
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		if err := server.Shutdown(); err != nil {
+			log.Error().Err(err).Msg("error during fasthttp server shutdown")
+		}
+		<-serveErrCh
+
+	case err := <-serveErrCh:
+		if err != nil {
+			log.Error().Err(err).Msg("fasthttp server failed unexpectedly")
+		}
+	}
+}
+
+func (h *RestHandler) healthCheck(ctx *fasthttp.RequestCtx) {
+	h.res.SendSuccess(ctx, fasthttp.StatusOK, "all ok")
+}
+
+func (h *RestHandler) getProduct(ctx *fasthttp.RequestCtx) {
+	user_id := middleware.GetUserIDFromCtx(ctx)
+
+	query := ctx.QueryArgs().Peek("query")
+
+	if len(query) == 0 {
+		h.res.SendSuccess(ctx, fasthttp.StatusOK, "nothing to search")
+	}
+
+	if user_id == "" {
+		h.log.Info().Msg("no user was found")
+	}
+	h.log.Info().Str("user_id", user_id).Msg("user found")
+
+	searchRes, err := h.index.Search(string(query), &meilisearch.SearchRequest{
+		Limit: 10,
+	})
+	if err != nil {
+		h.res.SendError(ctx, fasthttp.StatusInternalServerError, "failed to get products from the search")
+	}
+
+	h.res.SendSuccess(ctx, fasthttp.StatusOK, searchRes.Hits)
+}
